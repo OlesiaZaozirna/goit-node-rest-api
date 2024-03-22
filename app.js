7cf767403f05ba263fc3c1f5ffa6128131baa459
OlesiaZaozirna <137696365+OlesiaZaozirna@users.noreply.github.com>
2024-03-22 19:35:58 +0200

    add DB_HOST

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import mongoose from "mongoose";
+import { DB_HOST } from "./config.js";
 
 import contactsRouter from "./routes/contactsRouter.js";
 
@@ -21,18 +22,16 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
+console.log(process.env);
 
-const DB_HOST =
-  "mongodb+srv://OlesiaZaozirna:747459Lesyapka@cluster0.ctpfd9h.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=Cluster0";
-
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(3000, () => {
-      console.log("Database connection successful. Use our API on port: 3000");
-    });
-  })
-  .catch((error) => {
-    process.exit(1);
-    console.error(error.message);
-  });
+//mongoose
+// .connect(DB_HOST)
+//  .then(() => {
+//    app.listen(3000, () => {
+//     console.log("Database connection successful. Use our API on port: 3000");
+//    });
+//  })
+//  .catch((error) => {
+//    process.exit(1);
+//    console.error(error.message);
+//  });
